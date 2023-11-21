ce6f9502805a36c2af2732f1667fd32b194e58eb
7moon120 <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2023-11-21 23:06:56 +0800

    Update MainActivity.kt

--- ColorTitles/MainActivity.kt
+++ ColorTitles/MainActivity.kt
@@ -15,18 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Инициализация массива тайлов с использованием их ID
+
         tiles = arrayOf(
             arrayOf(findViewById(R.id.t00), findViewById(R.id.t01), findViewById(R.id.t02), findViewById(R.id.t03)),
             arrayOf(findViewById(R.id.t10), findViewById(R.id.t11), findViewById(R.id.t12), findViewById(R.id.t13)),
             arrayOf(findViewById(R.id.t20), findViewById(R.id.t21), findViewById(R.id.t22), findViewById(R.id.t23)),
             arrayOf(findViewById(R.id.t30), findViewById(R.id.t31), findViewById(R.id.t32), findViewById(R.id.t33))
         )
-        // Заполнение поля случайными цветами
+
         initField()
     }
 
-    // Функция для получения координат из строки
+
     fun getCoordFromString(s: String): Coord {
         val x = s[0].toString().toInt()
         val y = s[1].toString().toInt()
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         return Coord(x, y)
     }
 
-    // Функция для изменения цвета тайла
+
     fun changeColor(view: View) {
         val brightColor = resources.getColor(R.color.bright, theme)
         val darkColor = resources.getColor(R.color.dark, theme)
@@ -52,50 +52,42 @@ class MainActivity : AppCompatActivity() {
         val coord = getCoordFromString(v.tag.toString())
         changeColor(v)
 
-        // Изменение цвета всех тайлов вокруг выбранного тайла
         for (i in 0..3) {
             changeColor(tiles[coord.x][i])
             changeColor(tiles[i][coord.y])
         }
 
-        // Проверка наличия победы
         checkVictory()
     }
 
-    // Функция для проверки победы
+
     fun checkVictory() {
-        // Предположим, что победа достигается, если все тайлы одного цвета
+
         val brightColor = resources.getColor(R.color.bright, theme)
 
         for (i in 0..3) {
             for (j in 0..3) {
                 val drawable = tiles[i][j].background as ColorDrawable
                 if (drawable.color != brightColor) {
-                    // Если хотя бы один тайл не является ярким, игра не выиграна
                     return
                 }
             }
         }
 
-        // Если все тайлы являются яркими, выигрыш
+
         println("Victory!")
     }
 
-    // Инициализация поля случайными плитками
     fun initField() {
         val random = java.util.Random()
 
         for (i in 0..3) {
             for (j in 0..3) {
-                // Генерация случайного числа (0 или 1)
                 val randomValue = random.nextInt(2)
 
-                // Задание цвета тайла в зависимости от случайного числа
                 if (randomValue == 0) {
-                    // Темный цвет
                     tiles[i][j].setBackgroundColor(resources.getColor(R.color.dark, theme))
                 } else {
-                    // Светлый цвет
                     tiles[i][j].setBackgroundColor(resources.getColor(R.color.bright, theme))
                 }
             }
